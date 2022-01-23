a289a7bf1fcd3f16d36d4b4a028d80544a84d91b
Ishraq <ishraq.h.c@gmail.com>
2022-01-23 15:12:50 +0100

    created the weather data fetch code in python

---
+++ README.md
@@ -0,0 +1,6 @@
+# Weather Fetching in Python
+
+In this python code example, we can get the weather update of almost any city in the world. All you have to do is enter the name of the city and pass it as query in the openweather API URL and you will get the result in the form of json.
+
+Weather API website: https://openweathermap.org/
+Tutorial link: Coming soon
---
+++ script.py
@@ -0,0 +1,24 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+BASE_URL = os.getenv('BASE_URL')
+
+city = input('Enter the name of the city: ')
+request_url = f"{BASE_URL}?appid={API_KEY}&q={city}"
+response = requests.get(request_url)
+
+if response.status_code == 200:
+    data = response.json()
+    print(data)
+
+    weather = data['weather'][0]['description']
+    temperature = data["main"]["temp"] - 273
+    print('Weather: ', weather)
+    print('Temperature: ', temperature, '\N{DEGREE SIGN}C')
+
+else:
+    print('There might be an error')
